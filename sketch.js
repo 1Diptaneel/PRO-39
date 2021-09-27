940ec0167db809fdb44711624d9b1783674a2bc7
1Diptaneel <83410205+1Diptaneel@users.noreply.github.com>
2021-09-27 21:22:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -193,7 +193,7 @@ function spawnObstacles() {
   }
 }
 
-/*function reset(){
+function reset(){
   gameState = PLAY;
   gameOver.visible = true;
   restart.visible = true;
@@ -203,7 +203,7 @@ function spawnObstacles() {
   obstaclesGroup.Each();
   shrubsGroup.destroyEach();
   score = 0;
-}*/
+}
 
 /*function reset(){
   gameState = PLAY;
@@ -217,7 +217,7 @@ function spawnObstacles() {
 
 }*/
 
-function reset(){
+/*function reset(){
   gameState = PLAY;
   gameOver.visible = false;
   restart.visible = false;
@@ -227,7 +227,7 @@ function reset(){
   obstaclesGroup.destroyeach();
   shrubsGroup.destroyeach();
   score = 0;
-}
+}*/
 
 /*function reset(){
   gameState = PLAY;
